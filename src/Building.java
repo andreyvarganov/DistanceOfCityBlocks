eae1969c8816c29013bb5b18e0a1cfba39ea516d
InLoveWithJava <javalang@list.ru>
2020-03-07 13:05:49 +0300

    Adding a .gitignore file

---
+++ src/Building.java
@@ -0,0 +1,9 @@
+public class Building {
+
+    private static int quantity; // количество зданий
+    private int x, y; // координаты здания
+
+    public Building(int x, int y) {
+
+    }
+}
---
+++ src/Canvas.java
@@ -0,0 +1,11 @@
+public class Canvas {
+
+    private static int N; // ширина поля;
+    private static int M; // длина поля;
+
+    public Canvas(int N, int M) {
+        Canvas.N = N;
+        Canvas.M = M;
+    }
+
+}
