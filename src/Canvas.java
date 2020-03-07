530096145805e51174f43aca6e34d2343efa3898
InLoveWithJava <javalang@list.ru>
2020-03-07 14:08:28 +0300

    #2 - Drawing and printing canvas are realised

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,11 +1,57 @@
 public class Canvas {
 
-    private static int N; // ширина поля;
-    private static int M; // длина поля;
+    /**
+     * Класс-холст для отрисовки
+     */
 
-    public Canvas(int N, int M) {
-        Canvas.N = N;
-        Canvas.M = M;
+    // ширина и высота
+    private int width;
+    private int height;
+    // матрица, на которой отрисовываем объекты
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Обозначение объектов:
+     * 🏠 (1) - здание;
+     * 🚧 (2) - перекрытие дороги;
+     * ― (-3) - горизонтальная граница отрисовки;
+     * 🚓 (4) - машина с хладогентами (место отправления, "Home";
+     * * (5) - проеханный путь;
+     *  (0) - пустая клетка;
+     *  ➕ (6) - пункт назначения;
+     */
+
+    /**
+     * * Помещаем объект на карту: (x, y) - координаты, c - объект
+     */
+
+    void setPoint(int x, int y, char c) {
+        if (y < 0 || y >= matrix.length) return;
+        if (x < 0 || x >= matrix[0].length) return;
+        matrix[y][x] = c;
+    }
+
+    /**
+     * Вывод на экран
+     */
+
+    void print() {
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
     }
 
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,53 @@
-public class Solution {
-    public static void main(String[] args) {
+import java.util.List;
 
+public class Solution {
+
+    public static Solution app;
+
+    private int N; // размерность поля
+
+    private List<Building> buildings; // список зданий
+
+    private Home home; // место отправления
+
+    private School school; // место назначения
+
+
+    public Solution(int N) {
+        this.N = N;
+    }
+
+    public void draw(Canvas canvas) {
+        drawBorder(canvas);
+    }
+
+    private void drawBorder(Canvas canvas) {
+        for (int i = 0; i < N + 2; i++) {
+            for (int j = 0; j < N + 2; j++) {
+                canvas.setPoint(i, j, ' ');
+            }
+        }
+
+        for (int i = 0; i < N + 2; i++) {
+            canvas.setPoint(i, 0, '―');
+            canvas.setPoint(i, N + 1, '―');
+        }
+
+        for (int i = 0; i < N + 2; i++) {
+            canvas.setPoint(0, i, '|');
+            canvas.setPoint(N + 1, i, '|');
+        }
+    }
+
+    private void run() {
+        // создаем холст для отрисовки
+        Canvas canvas = new Canvas(N, N);
+        draw(canvas);
+        canvas.print();
+    }
+
+    public static void main(String[] args) {
+        app = new Solution(10);
+        app.run();
     }
 }
